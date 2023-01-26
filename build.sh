0caf0ee00f1510d41fc68edb536ee45972146ddd
Robin Appelman <robin@icewind.nl>
2023-01-26 15:21:57 +0100

    split docker build in ci

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t icewind1991/samba-krb-test-apache apache-krb
+docker build -t icewind1991/samba-krb-test-apache apache
 docker build -t icewind1991/samba-krb-test-apache-gssapi apache-gssapi
 
 docker build -t icewind1991/samba-krb-test-client client
