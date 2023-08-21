9f64b78b76bd9a75a323bab33f15c618c1d5aeb7
Robin Appelman <robin@icewind.nl>
2023-08-21 16:06:48 +0200

    move kerb protection to subfolder

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ echo "DC: $DC_IP"
 SERVER_IP=$(./start-server.sh "$DC_IP" /srv/http/smb "$MODE")
 echo "SERVER: $SERVER_IP"
 
-LIST=$(./client-cmd.sh "$DC_IP" curl --negotiate -u testuser@DOMAIN.TEST: --delegation always http://httpd.domain.test/example-sso-kerberos.php)
+LIST=$(./client-cmd.sh "$DC_IP" curl --negotiate -u testuser@DOMAIN.TEST: --delegation always http://httpd.domain.test/kerb/example-sso-kerberos.php)
 
 echo $LIST
 
