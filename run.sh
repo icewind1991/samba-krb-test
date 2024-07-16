2f6a07b9107f15e9504e6ddc99d7251f5f8dcae2
Robin Appelman <robin@icewind.nl>
2024-07-16 13:40:22 +0200

    attempt to fix test

--- run.sh
+++ run.sh
@@ -5,9 +5,10 @@ MODE=${1:-"apache"}
 DC_IP=$(./start-dc.sh)
 
 echo "DC: $DC_IP"
+PHP_VERSION=${2:-"8.1"}
 
 # start server
-SERVER_IP=$(./start-server.sh "$DC_IP" /srv/http/smb "$MODE")
+SERVER_IP=$(./start-server.sh "$DC_IP" /srv/http/smb "$MODE" "$PHP_VERSION")
 echo "SERVER: $SERVER_IP"
 
 LIST=$(./client-cmd.sh "$DC_IP" curl --negotiate -u testuser@DOMAIN.TEST: --delegation always http://httpd.domain.test/kerb/example-sso-kerberos.php)
--- start-server.sh
+++ start-server.sh
@@ -5,7 +5,7 @@ docker rm -f server 2>/dev/null > /dev/null
 MODE=${3:-"apache"}
 PHP_VERSION=${4:-"8.1"}
 
-docker run -d --name server -v $2:/var/www/html -v /tmp/shared:/shared --dns $1 --hostname httpd.domain.test "icewind1991/samba-krb-test-$MODE" 1>&2
+docker run -d --name server -v $2:/var/www/html -v /tmp/shared:/shared --dns $1 --hostname httpd.domain.test "icewind1991/samba-krb-test-$MODE:$PHP_VERSION" 1>&2
 SERVER_IP=$(docker inspect server --format '{{.NetworkSettings.IPAddress}}')
 
 # add the dns record for server
